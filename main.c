2d7b84115bfc09efe73e454cb0268c8076f5157d
HosseinArianmehr2004 <hossein.arianmehr2004@gmail.com>
2024-12-05 00:48:12 +0330

    create 3 main processes

--- main.c
+++ main.c
@@ -3,27 +3,33 @@
 #include <string.h>
 #include <unistd.h>
 
-int main()
-{
-    char username[100]; // an array of chars is an string :/
+char username[100];       // an array of chars is an string :/
+char order_list[100][50]; // Array with 100 rows of strings, each with length 50
 
+void login()
+{
     printf("Username: ");
     scanf("%99s", username);
 
     char file_path[200];
     strcpy(file_path, "Users/");
     strcat(file_path, username);
-    strcat(file_path, ".log");
+    strcat(file_path, ".txt");
 
     FILE *file;
     file = fopen(file_path, "w");
     if (file == NULL)
     {
         perror("Error creating file");
-        return EXIT_FAILURE;
+        // return EXIT_FAILURE;
     }
+    fprintf(file, "Username: %s\n", username);
+    fprintf(file, "Purchase Times: %d", 0);
+    fclose(file);
+}
 
-    char order_list[100][50]; // Array with 100 rows of strings, each with length 50
+void get_order_list()
+{
     printf("Orderlist0:\n");
 
     char input[50];
@@ -40,6 +46,26 @@ int main()
     int threshold;
     printf("Price threshold: ");
     scanf("%d", &threshold);
+}
+
+int main()
+{
+    // login();
+    // get_order_list();
+
+    int pid1 = fork();
+    int pid2 = fork();
+
+    if (pid1 != 0 && pid2 != 0)
+    {
+        // Parent process
+        printf("parent %d %d\n", pid1, pid2);
+    }
+    else
+    {
+        // All 3 childs and grandchilds
+        printf("child %d %d\n", pid1, pid2);
+    }
 
     return 0;
 }
